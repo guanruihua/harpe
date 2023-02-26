6a431ad51cd6bae264af45e0a70b62ff220a219c
grh-gitee <3494242908@qq.com>
2023-02-26 09:36:57 +0800

    chore: 优化包打包配置

--- config/rollup.build.js
+++ config/rollup.build.js
@@ -1,17 +1,21 @@
-import path from 'path'
-import rollupTypescript from 'rollup-plugin-typescript2'
-import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import { eslint } from 'rollup-plugin-eslint'
-import { terser } from 'rollup-plugin-terser'
-import pkg from '../package.json'
+const path = require('path')
+const rollupTypescript = require('rollup-plugin-typescript2')
+const babel = require('rollup-plugin-babel')
+const resolve = require('rollup-plugin-node-resolve')
+const commonjs = require('@rollup/plugin-commonjs')
+const { eslint } = require('rollup-plugin-eslint')
+const { terser } = require('rollup-plugin-terser')
 
-const paths = {
-  input: path.join(__dirname, '../src/index.ts'),
-  output: path.join(__dirname, '../lib')
+const pkg = {
+  name: 'browser-helper-js'
 }
 
+const paths = {
+  // input: '../src/index.ts',
+  input: path.join(__dirname, '../src/index.ts'),
+  output: path.join(__dirname, '../lib')
+  // output: '../lib'
+}
 // rollup 配置项
 const rollupConfig = {
   input: paths.input,
@@ -21,14 +25,14 @@ const rollupConfig = {
       file: path.join(paths.output, 'index.js'),
       format: 'cjs',
       name: pkg.name,
-      sourcemap: true
+      sourcemap: true,
     },
     // 输出 es 规范的代码
     {
       file: path.join(paths.output, 'index.esm.js'),
       format: 'esm',
       name: pkg.name,
-      sourcemap: true
+      sourcemap: true,
     }
   ],
   plugins: [
@@ -36,8 +40,8 @@ const rollupConfig = {
     eslint({
       throwOnError: true, // lint 结果有错误将会抛出异常
       throwOnWarning: true,
-      include: ['src/**/*.ts'],
-      exclude: ['node_modules/**', 'lib/**', '*.js', 'dist/**']
+      include: ['../src/**/*.ts'],
+      exclude: ['../node_modules/**', '../lib/**', '*.js', '../dist/**']
     }),
 
     // 使得 rollup 支持 commonjs 规范，识别 commonjs 规范的依赖
@@ -54,9 +58,9 @@ const rollupConfig = {
     babel({
       runtimeHelpers: true,
       // 只转换源代码，不运行外部依赖
-      exclude: 'node_modules/**',
-      tsconfig:'./tsconfig.json',
-      include: './src',
+      exclude: '../node_modules/**',
+      tsconfig: '../tsconfig.json',
+      include: '../src',
       // babel 默认不支持 ts 需要手动添加
       extensions: ['.ts']
     }),
@@ -65,4 +69,4 @@ const rollupConfig = {
   ]
 }
 
-export default rollupConfig
+module.exports = rollupConfig
